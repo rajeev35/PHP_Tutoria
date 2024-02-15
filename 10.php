6d7dab057381fc737ff4cb17de444a68e06cab8e
rajeev35 <rajeev2020it049@abesit.edu.in>
2024-02-15 14:30:00 +0530

    nasted if else conditional statement

---
+++ 10.php
@@ -0,0 +1,16 @@
+<?php
+
+$age = 67;
+
+if($age>=18){
+    if($age<65){
+        echo "Yes you can vaote";
+
+    }else{
+        echo "No you can not vote anymore";
+    }
+}else{
+    echo "You can not vote";
+}
+
+?>
