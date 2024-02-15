1d43215fd4eaa6b073eca65b01b16020105aa826
rajeev35 <rajeev2020it049@abesit.edu.in>
2024-02-15 14:18:09 +0530

    conditional operator

---
+++ 9.php
@@ -0,0 +1,16 @@
+<?php
+
+$age = 17;
+
+if($age == 18){
+    echo "Yes you can vote";
+
+}elseif($age==17){
+    echo "Please wait one more year";
+
+}else{
+    echo "You can't vote";
+    
+}
+
+?>
