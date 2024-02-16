c6e4acc14dbe6dabddc6b2cb7cafd63cfb9026b4
rajeev35 <rajeev2020it049@abesit.edu.in>
2024-02-16 10:57:47 +0530

    multidimensional array

---
+++ multiarr13.php
@@ -0,0 +1,20 @@
+<?php
+
+$arr3 = array(
+
+    'name' => 'Rajeev Kumar',
+    'company'=>'Google',
+    'contact'=> array('8789242056','8789242056'),
+    'status'=>'false till now',
+    'age'=> 21,
+
+
+
+);
+
+echo '<pre>';
+
+print_r($arr3)
+
+
+?>
