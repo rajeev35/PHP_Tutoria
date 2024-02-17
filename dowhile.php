a8a72328517f689a80b2227b7dd3467adc6305f5
rajeev35 <rajeev2020it049@abesit.edu.in>
2024-02-17 15:33:39 +0530

    dowhile

---
+++ dowhile.php
@@ -0,0 +1,20 @@
+<?php
+
+  $i = 1;
+   while($i<10){
+
+    echo "Learn with Rajeev<br/>";
+
+    $i++;
+   }
+
+   do{
+
+    echo "Learn with Rajeev<br/>";
+
+    $j++;
+
+   }while($j<10)
+
+
+?>
