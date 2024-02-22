dbdba371c868cfcf302115b7f6eb708abffdd284
rajeev35 <rajeev2020it049@abesit.edu.in>
2024-02-22 14:34:14 +0530

    Upper lower case handeling

---
+++ uplowcase.php
@@ -0,0 +1,18 @@
+<?php
+
+$str = "sDhsfDF";
+
+echo $str;
+echo "<pre>";
+
+echo strtolower($str). "<br/>";
+echo strtoupper($str). "<br/>";
+
+echo lcfirst($str). "<br/>";
+
+$str2 = "rajeev";
+
+echo ucfirst($str2)."<br/>";
+
+
+?>
