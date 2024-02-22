5a3fd024141bb7e81fd7c6dddbed188df0024bbf
rajeev35 <rajeev2020it049@abesit.edu.in>
2024-02-22 14:33:49 +0530

    Implode and  Explode function

---
+++ impexl.php
@@ -0,0 +1,40 @@
+<?php
+
+// $arr = [
+//     "Hello",
+//     "Everyone",
+//     "How",
+    
+
+//     "are",
+//     "You"
+// ];
+
+
+
+// echo "<pre>";
+// print_r($arr);
+
+// $str = implode(" ",$arr);
+
+// echo $str;
+
+//Array to  string conversion;
+
+
+$str = "A,B,C,D,E,F";
+
+echo $str;
+
+$arr = explode(',',$str);
+
+
+echo "<pre>";
+
+print_r($arr);
+
+
+//string to array conversion;
+
+
+?>
